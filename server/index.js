afb0c9dd176f507d6febb4e028be6d4e67f97896
Vinayak-katkar <24071A66H3@vnrvjiet.in>
2026-04-08 20:52:55 +0530

    Hello

--- server/index.js
+++ server/index.js
@@ -207,6 +207,10 @@ app.get("/api/leaderboard", requireJuryAccess, async (_req, res) => {
   return res.json({ leaderboardByDomain });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+if (!process.env.VERCEL) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
